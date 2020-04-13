da7442acaca77a212dedad679623e4c317683024
Gaëtan Bloch <gaetan.bloch@gmail.com>
2020-04-13 17:40:24 +0200

    Init Spring Boot Application

---
+++ src/main/java/guru/springframework/spring5mvcrest/Spring5MvcRestApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.spring5mvcrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5MvcRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring5MvcRestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/guru/springframework/spring5mvcrest/Spring5MvcRestApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.spring5mvcrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5MvcRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
