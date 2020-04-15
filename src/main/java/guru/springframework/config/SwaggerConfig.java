843f3c4929202f70793f40810f09067a7877ebe4
Gaëtan Bloch <gaetan.bloch@gmail.com>
2020-04-15 17:33:47 +0200

    Added Swagger configuration

---
+++ src/main/java/guru/springframework/config/SwaggerConfig.java
@@ -0,0 +1,29 @@
+package guru.springframework.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * @author Gaetan Bloch
+ * Created on 15/04/2020
+ */
+@EnableSwagger2
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api() {
+        // Default configuration
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build()
+                .pathMapping("/");
+    }
+}
